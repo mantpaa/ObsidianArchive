23f8812feb9a9e57c2297db8bfef3386ddb7a9e7
mantpaa <mantpaa@gmail.com>
2026-06-29 19:15:54 +0200

    Added EntityFramework, SqlServer and EF.Tools. Addid applicationDbContext with DbSet for Category.

---
+++ ObsidianArchive/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ObsidianArchiveWeb.Models;
+
+namespace ObsidianArchiveWeb.ApplicationDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
