f3e7611ed0ea5eab977562ba1faf53bd60397566
mantpaa <mantpaa@gmail.com>
2026-06-29 18:33:17 +0200

    Add project files.

---
+++ ObsidianArchive/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ObsidianArchive.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
