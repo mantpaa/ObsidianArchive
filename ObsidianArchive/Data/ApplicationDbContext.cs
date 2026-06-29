f9abd1e5166a614b80e91e2cd22e495291afe03e
mantpaa <mantpaa@gmail.com>
2026-06-29 19:39:59 +0200

    Seeded Category table and migration.

--- ObsidianArchive/Data/ApplicationDbContext.cs
+++ ObsidianArchive/Data/ApplicationDbContext.cs
@@ -8,5 +8,15 @@ namespace ObsidianArchiveWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Mystery" },
+                new Category { Id = 2, Name = "Romance" },
+                new Category { Id = 3, Name = "Thriller" }
+                );
+        }
     }
 }
