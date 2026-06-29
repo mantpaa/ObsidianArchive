5e54b32cdb07b2e65e7b602b24ee179efb0b165b
mantpaa <mantpaa@gmail.com>
2026-06-29 19:53:31 +0200

    Add CategoryController and related Index view.

---
+++ ObsidianArchive/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ObsidianArchiveWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
