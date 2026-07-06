216d5c67f6742c46daa9637553954fa7b3ea3341
mantpaa <mantpaa@gmail.com>
2026-07-06 13:44:00 +0200

    Update, Delete endpoints. Toastr notifications for successful CRUD operations.

--- ObsidianArchive/Controllers/CategoryController.cs
+++ ObsidianArchive/Controllers/CategoryController.cs
@@ -39,11 +39,84 @@ namespace ObsidianArchiveWeb.Controllers
             {
                 _context.Categories.Add(category);
                 _context.SaveChanges();
+                TempData["success"] = "Category created";
                 return RedirectToAction("Index");
             }
             return View();
             
         }
 
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Update")]
+        public IActionResult UpdatePost(Category category)
+        {
+            if (!string.IsNullOrEmpty(category.Name) && _context.Categories.Any(c=> c.Name.ToLower() == category.Name.ToLower() && c.Id != category.Id))
+            {
+                ModelState.AddModelError("", "Category name must be unique.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category updated";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category deleted";
+            return RedirectToAction("Index");
+        }
     }
 }
