7f373e256ce9549501e1f830413cb36a30c55d15
mantpaa <mantpaa@gmail.com>
2026-07-07 15:57:05 +0200

    Added Product table to database. Added migration for Product table and seed data.

--- ObsidianArchive.DataAccess/Data/ApplicationDbContext.cs
+++ ObsidianArchive.DataAccess/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace ObsidianArchive.DataAccess.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -16,6 +17,36 @@ namespace ObsidianArchive.DataAccess.Data
                 new Category { Id = 1, Name = "Mystery", DisplayOrder=1 },
                 new Category { Id = 2, Name = "Romance", DisplayOrder=2},
                 new Category { Id = 3, Name = "Thriller", DisplayOrder=3 }
+                );
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Title = "Compendium of the Occult", Description = "Compendium of the OccultIn seven themed chapters, the book examines the use of talismans and charms, the practice of casting curses, secret societies and sacred sites, divination, rites, and rituals across the world, starting with an introduction to occult practices.",
+                    ISBN = "9780500028148", Author="Liz Williams", ListPrice = "37.95$", Price = 30, Price50 = 25, Price100 = 20, ImageUrl = ""  },
+                new Product
+                {
+                    Id = 2,
+                    Title = "another book",
+                    Description = "A nother book description",
+                    ISBN = "9780500028139",
+                    Author = "Made up author",
+                    ListPrice = "37.95$",
+                    Price = 30,
+                    Price50 = 25,
+                    Price100 = 20,
+                    ImageUrl = ""
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "third book",
+                    Description = "description here",
+                    ISBN = "9780500028200",
+                    Author = "Made up author2",
+                    ListPrice = "37.95$",
+                    Price = 30,
+                    Price50 = 25,
+                    Price100 = 20,
+                    ImageUrl = ""
+                }
                 );
         }
     }
---
+++ ObsidianArchive.DataAccess/Migrations/20260707135628_AddProductTableAndSeedData.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ObsidianArchive.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductTableAndSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DisplayOrder",
+                table: "Categories",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ImageUrl", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Liz Williams", "Compendium of the OccultIn seven themed chapters, the book examines the use of talismans and charms, the practice of casting curses, secret societies and sacred sites, divination, rites, and rituals across the world, starting with an introduction to occult practices.", "9780500028148", "", "37.95$", 30.0, 20.0, 25.0, "Compendium of the Occult" },
+                    { 2, "Made up author", "A nother book description", "9780500028139", "", "37.95$", 30.0, 20.0, 25.0, "another book" },
+                    { 3, "Made up author2", "description here", "9780500028200", "", "37.95$", 30.0, 20.0, 25.0, "third book" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DisplayOrder",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
