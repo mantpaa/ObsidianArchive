53ad097f4b00f116cb6771ce37d6a1cc8e9fddcb
mantpaa <mantpaa@gmail.com>
2026-08-08 19:53:54 +0200

    Added layout for admin.

---
+++ ObsidianArchive/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ObsidianArchive.Utility;
+
+namespace ObsidianArchiveWeb.Areas.Admin.Controllers
+{
+    [Authorize(Roles = StaticDetails.RoleAdmin)]
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
